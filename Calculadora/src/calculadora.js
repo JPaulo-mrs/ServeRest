fdeaf7eb31495076865fa95cab90b8c7bf3c7be2
João Paulo <joaopaulodemourasjp@gmail.com>
2024-09-06 10:20:01 -0300

    Att 'calculadora.js', acrescentado testes novos

--- Calculadora/src/calculadora.js
+++ Calculadora/src/calculadora.js
@@ -2,36 +2,49 @@
 export default class Calculadora{
     static soma(a,b)
     {
-        return a+b;
+        if(typeof a === 'number' && typeof b === 'number') return a+b;
+        else return 'Tipo da variável não é numérico';
+        
     }
     static subtracao(a,b)
     {
-        return a-b;
+        if(typeof a === 'number' && typeof b === 'number') return a-b;
+        else return 'Tipo da variável não é numérico';
     }
     static divisao(a,b)
     {
-        return a/b;
+        if(typeof a === 'number' && typeof b === 'number') return a/b;
+        else return 'Tipo da variável não é numérico';
     }
     static multiplicacao(a,b)
     {
-        return a*b;
+        if(typeof a === 'number' && typeof b === 'number') return a*b;
+        else return 'Tipo da variável não é numérico';
     }
     static fatorial(a)
     {
-        if(a!==1)
+        if(typeof a === 'number')
         {
-            return a*this.fatorial(a-1);
+            if(a!==1)
+            {
+                return a*this.fatorial(a-1);
+            }
+            else
+            {
+                return a;
+            }
         }
-        else
-        {
-            return a;
-        }
+        else return 'Tipo da variável não é numérico';
     }
     static potenciacao(a, b) {
         let resultado = 1;
-        for (let i = 0; i < b; i++) {
-            resultado *= a;
+        if(typeof a === 'number' && typeof b === 'number')
+        {
+            for (let i = 0; i < b; i++) {
+                resultado *= a;
+            }
+            return resultado;
         }
-        return resultado;
+        else return 'Tipo da variável não é numérico';
     }
 }
--- Calculadora/test/calculadora.spec.js
+++ Calculadora/test/calculadora.spec.js
@@ -1,65 +1,69 @@
 import assert from 'assert'
 import {expect} from 'chai'
-import calculadora from '../src/calculadora.js'
+import Calculadora from '../src/calculadora.js'
 
 
 
 describe('Testes de adição',()=>{
     it('Deve somar 4 e 5 resultando em 9', ()=>{
-        let resultado = calculadora.soma(4,5)
+        let resultado = Calculadora.soma(4,5)
         expect(resultado).to.be.eq(9)
     })
-    it('Deve somar -4 e 5 resultando em 1', ()=>{
-        let resultado = calculadora.soma(-4,5)
-        expect(resultado).to.be.eq(1)
+    it('Deve verificar se as variaveis são do tipo numérico', ()=>{
+        let resultado = Calculadora.soma(4,'5')
+        expect(resultado).to.be.eq('Tipo da variável não é numérico')
     })
 })
 
 describe('Testes de subtração',()=>{
     it('Deve subtrair 4 e 5 resultando em -1', ()=>{
-        let resultado = calculadora.subtracao(4,5)
+        let resultado = Calculadora.subtracao(4,5)
         expect(resultado).to.be.eq(-1)
     })
-    it('Deve subtrair -4 e 5 resultando em -9', ()=>{
-        let resultado = calculadora.subtracao(-4,5)
-        expect(resultado).to.be.eq(-9)
+    it('Deve verificar se as variaveis são do tipo numérico', ()=>{
+        let resultado = Calculadora.subtracao(4,'5')
+        expect(resultado).to.be.eq('Tipo da variável não é numérico')
     })
 })
 
 describe('Testes de divisão',()=>{
     it('Deve dividir 4 por 2 resultando em 2', ()=>{
-        let resultado = calculadora.divisao(4,2)
+        let resultado = Calculadora.divisao(4,2)
         expect(resultado).to.be.eq(2)
     })
     it('Deve dividir 5 por 2 resultando em 2.5', ()=>{
-        let resultado = calculadora.divisao(5,2)
+        let resultado = Calculadora.divisao(5,2)
         expect(resultado).to.be.eq(2.5)
     })
-    it('Deve dividir -4 por 2 resultando em -2', ()=>{
-        let resultado = calculadora.divisao(-4,2)
-        expect(resultado).to.be.eq(-2)
+    it('Deve verificar se as variaveis são do tipo numérico', ()=>{
+        let resultado = Calculadora.divisao(4,'5')
+        expect(resultado).to.be.eq('Tipo da variável não é numérico')
     })
 })
 
 describe('Testes de multiplicação',()=>{
     it('Deve multiplicar 4 por 5 resultando em 20', ()=>{
-        let resultado = calculadora.multiplicacao(4,5)
+        let resultado = Calculadora.multiplicacao(4,5)
         expect(resultado).to.be.eq(20)
     })
     it('Deve multiplicar -4 por 5 resultando em -20', ()=>{
-        let resultado = calculadora.multiplicacao(-4,5)
+        let resultado = Calculadora.multiplicacao(-4,5)
         expect(resultado).to.be.eq(-20)
     })
-    it('Deve multiplicar 2.5 por 2 resultando em 5', ()=>{
-        let resultado = calculadora.multiplicacao(2.5,2)
-        expect(resultado).to.be.eq(5)
+    it('Deve verificar se as variaveis são do tipo numérico', ()=>{
+        let resultado = Calculadora.multiplicacao(4,'5')
+        expect(resultado).to.be.eq('Tipo da variável não é numérico')
     })
 })
 
 describe('Testes de fatorial',()=>{
     it('Deve realizar o fatorial de 5 resultando em 120', ()=>{
-        let resultado = calculadora.fatorial(5)
+        let resultado = Calculadora.fatorial(5)
         expect(resultado).to.be.eq(120)
+    })
+    it('Deve verificar se as variaveis são do tipo numérico', ()=>{
+        let resultado = Calculadora.fatorial('5')
+        expect(resultado).to.be.eq('Tipo da variável não é numérico')
     })
 })
 
@@ -67,5 +71,9 @@ describe('Testes de potenciação',()=>{
     it('Deve elevar 5 a 2 resultando em 25', ()=>{
         let resultado = calculadora.potenciacao(5, 2)
         expect(resultado).to.be.eq(25)
+    })
+    it('Deve verificar se as variaveis são do tipo numérico', ()=>{
+        let resultado = calculadora.potenciacao(4,'5')
+        expect(resultado).to.be.eq('Tipo da variável não é numérico')
     })
 })
